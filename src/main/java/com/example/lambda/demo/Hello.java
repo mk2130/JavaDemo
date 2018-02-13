2b042d46fb98965f1ec290b9e6731feefb260a90
mk2130 <36299792+mk2130@users.noreply.github.com>
2018-02-13 14:12:50 +0530

    Update Hello.java

--- src/main/java/com/example/lambda/demo/Hello.java
+++ src/main/java/com/example/lambda/demo/Hello.java
@@ -3,10 +3,10 @@ package com.example.lambda.demo;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Hello implements RequestHandler<String, Context> {
+public class Hello implements RequestHandler<String, String> {
 
 	@Override
-	public String handleRequest(String input,Context context) {
+	public String handleRequest(String input, Context context) {
 	  String output = "Hello mayur !";
 	  return output;
 	}
