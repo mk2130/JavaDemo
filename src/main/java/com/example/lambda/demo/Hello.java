c945899b77cb95ac50a77242e4399c388c2efa1d
MAYUR KHAIRNAR <MAYUR.KHAIRNAR@lntinfotech.com>
2018-02-13 15:29:20 +0530

    updated template2

--- src/main/java/com/example/lambda/demo/Hello.java
+++ src/main/java/com/example/lambda/demo/Hello.java
@@ -7,12 +7,8 @@ public class Hello implements RequestHandler<String, String> {
 
 	@Override
 	public String handleRequest(String input, Context context) {
-<<<<<<< HEAD
-	  context.getLogger().log("Input: " + input);
-	  String output = "Hello, " + input + "!";
-=======
+		
 	  String output = "Hello mayur !";
->>>>>>> 3bd5fbf2171250de7f812e1e2ac35196069d6116
 	  return output;
 	}
 
